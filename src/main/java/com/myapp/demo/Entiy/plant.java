099089fd5a7389b1db86c4ab29327b74287e428d
shengdiya <91327731+shengdiya@users.noreply.github.com>
2023-12-05 09:18:07 +0800

    第二次修改测试

---
+++ src/main/java/com/myapp/demo/Entiy/plant.java
@@ -0,0 +1,9 @@
+package com.myapp.demo.Entiy;
+
+public class plant {
+    private Integer plantId; //植物Id
+    private String plantName; //
+    private String feature;
+    private String cultivation;
+    private String value;
+}
